03dda538857473d5944c16952c9d34b926fa6688
Ravindu <chamathsamaraweera@gmail.com>
2020-12-05 23:28:37 +0530

    Updates

---
+++ precache-manifest.cfca75d98b278cf542986607ee73d164.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df6c2ce35bca206b2a399df4e13ef0a6",
+    "url": "/payment-chart/index.html"
+  },
+  {
+    "revision": "83a0a90d417fea62c44a",
+    "url": "/payment-chart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/payment-chart/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/payment-chart/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a0a90d417fea62c44a",
+    "url": "/payment-chart/static/js/main.855c87e8.chunk.js"
+  },
+  {
+    "revision": "4c6a15831122c08af5d3",
+    "url": "/payment-chart/static/js/runtime-main.e27787d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/payment-chart/static/media/logo.5d5d9eef.svg"
+  }
+]);
